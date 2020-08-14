dfea985518bebf5b7ae8f34da67d525917e6fde4
Matt Sullivan <msullivanxi@gmail.com>
2020-08-14 19:44:21 -0400

    removed the extra utf8 I had

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,6 @@ export default function Main() {
   return (
     <div className={styles.container}>
       <Head>
-        <meta charset="UTF-8" />
         <meta name="description" content="Matt Sullivan's personal portfolio site" />
         <meta name="keywords" content="designer, product design, design systems, matt sullivan" />
         <meta name="author" content="Matt Sullivan" />
--- pages/projects/pinnacle.js
+++ pages/projects/pinnacle.js
@@ -10,7 +10,6 @@ export default function Main() {
   return (
     <div id='projects' className={styles.container}>
       <Head>
-        <meta charset="UTF-8" />
         <meta name="description" content="Matt Sullivan's personal portfolio site" />
         <meta name="keywords" content="designer, product design, design systems, matt sullivan" />
         <meta name="author" content="Matt Sullivan" />
