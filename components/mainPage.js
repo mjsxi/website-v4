be4ee44feded3b2ed6db1e0ac28c40e1f46af64b
Matt Sullivan <msullivanxi@gmail.com>
2020-08-14 20:51:33 -0400

    reworked the intro and links now open new tabs

--- components/mainPage.js
+++ components/mainPage.js
@@ -5,7 +5,7 @@ import styles from '../styles/main.module.scss'
 const MainPage = () => (
   <div>
     <div className={styles.heading}>
-      <h1><span>matt sullivan</span> is a product designer in NYC focusing on building out design systems and building up teams to achieve success with empathy, understanding, and support.</h1>
+      <h1><span>hi im matt sullivan</span>, a product designer in nyc, my main focus is around empowering teams and building out design systems. I enjoy helping others, and designing products through listening and empathy.</h1>
     </div>
     <div className={styles.subhead}>
       <div className={styles.projects}>
@@ -30,8 +30,8 @@ const MainPage = () => (
       </div>
       <div className={styles.about}>
         <h2>about</h2>
-        <p>A little about me, I’m more of a cat person and have been living with my long term roomate <a href="//instagram.com/uglybabykai/">Kai</a> for the last 5 years, I enjoy reading books (mostly on <a href="//amazon.com/Very-Serious-Cookbook-Contra-Wildair/dp/071487602X">cooking</a> and <a href="https://shop.butterandscotch.com/collections/swag/products/signed-butter-scotch-cookbook">baking</a>) and finding weird books to get design inspiration from (shout out to <a href="//vimeo.com/55449250">Atlas of the New Dutch Water Defence Line</a>), really enjoy video games specifically life simulators and collector games (enjoy this <a href="//polygon.com/2014/8/21/6052553/how-to-catch-every-pokemon">story</a> of me as a youth playing Pokémon).</p>
-        <p>You can find me on <a href="//twitter.com/mjsxi">Twitter</a> (where I'm currently tweeting about Animal Crossing), <a href="//instagram.com/mjsxi/">Insta</a> (where I gram occationally), and if you’re here for it <a href="//linkedin.com/in/matt-sullivan-5455656a/">Linkedin</a>.</p>
+        <p>A little about me, I’m more of a cat person and have been living with my long term roomate <a target="_blank" href="//instagram.com/uglybabykai/">Kai</a> for the last 5 years, I enjoy reading books (mostly on <a target="_blank" href="//amazon.com/Very-Serious-Cookbook-Contra-Wildair/dp/071487602X">cooking</a> and <a target="_blank" href="https://shop.butterandscotch.com/collections/swag/products/signed-butter-scotch-cookbook">baking</a>) and finding weird books to get design inspiration from (shout out to <a target="_blank" href="//vimeo.com/55449250">Atlas of the New Dutch Water Defence Line</a>), really enjoy video games specifically life simulators and collector games (enjoy this <a target="_blank" href="//polygon.com/2014/8/21/6052553/how-to-catch-every-pokemon">story</a> of me as a youth playing Pokémon).</p>
+        <p>You can find me on <a target="_blank" href="//twitter.com/mjsxi">Twitter</a> (where I'm currently tweeting about Animal Crossing), <a target="_blank" href="//instagram.com/mjsxi/">Insta</a> (where I gram occationally), and if you’re here for it <a target="_blank" href="//linkedin.com/in/matt-sullivan-5455656a/">Linkedin</a>.</p>
       </div>
     </div>
   </div>
