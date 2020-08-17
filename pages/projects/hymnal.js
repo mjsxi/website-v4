5b54eea2ad704bd1762ff0446a36829c1a802639
Matt Sullivan <msullivanxi@gmail.com>
2020-08-17 01:07:16 -0400

    finsihed up hymnal

--- pages/projects/hymnal.js
+++ pages/projects/hymnal.js
@@ -46,74 +46,71 @@ export default function Main() {
             />
 
             <h3>hymnal</h3>
-            <h4>this project is for vox medias home grown ad building platform, all the ads built for concert and all sites on chorus used this tool to build and serve up ads to millions of users.</h4>
+            <h4>this brief is for vox media’s home grown ad building platform. all the ads were built for concert. all sites running on chorus used this tool to build and serve ads to millions of users.</h4>
 
-            <p>in 2016 the revenue product team set out to evaluate our internal tools and found one of the main product offerings had grown stale as a product and user flows were being affected negatively by it. The tool, which runs all of vox media's premium ads across multiple networks, had not grown with new and bigger demands and needed some rethinking to get things done in a more efficient and easier way.</p>
-            <p>the hope was to make creating and building ads much easier along with reducing the user friction at certain pain points in the creation by rethinking and modernizing the tool.</p>
+            <p>in 2016, the revenue product team set out to evaluate our internal tools and found one of the main product offerings had grown stale, as product and user flows were being affected negatively by it. the tool, which runs all of vox media's premium ads across multiple networks, was not meeting the demands of its users and needed to be updated to reflect its usage.</p>
+
+            <p>the goal was to make creating and building ads a better experience along with reducing user friction at certain pain points in the ad execution process by rethinking and modernizing the tool.</p>
 
             <img
               src="/assets/imgs/hymnal-ad-overview.jpg"
-              alt="the hymnal apps main screen"
+              alt="the hymnal apps main ad view"
             />
-            <cite>the main screen when looking at a hymnal made ad</cite>
+            <cite>the main view when looking at a hymnal made ad</cite>
 
             <h5>high level goals</h5>
 
             <p>
               <ul>
-                <li>reduce User Friction within the app</li>
-                <li>refresh User flow and rethink how new ads are built</li>
+                <li>reduce user friction within the app</li>
+                <li>refresh user flow and rethink how ads are built</li>
                 <li>reduce build times to decrease per ad cost</li>
               </ul>
             </p>
 
             <h5>jumping off point</h5>
 
-            <p>hymnal was brought into the world in 2014 and was a quick experiment by a few engineers as a proof of concept built using rails and bootstrap 2 without any design input (lolsob). Fast forward a few years and it was supporting nearly one hundred users and millions of dollars and grew to be one of vox media’s main lines of business. Due to user frustration hymnal was slated for a massive overhaul with a design lead rework.</p>
+            <p>hymnal was introduced in 2014 as a quick experiment as a small engineering project. it was initially built using rails and bootstrap without any design input. a few years later, it was supporting almost one hundred users and generating millions of dollars in revenue. as its needs expanded, hymnal was slated for a massive overhaul with a design-led focus.</p>
 
             <h5>initial findings and resolutions</h5>
 
-            <p>this project initially began as a simple refresh but once the team and I started talking to the users it soon came to light that the very nature of how we build ads was the biggest pain point — from uploading assets to managing them, it was all a mess. We started by asking a small sample of users to go through their workflow and noticed many of them weren’t aware of what they could edit within any given ad or how to manage assets — a design failure of the tool, as this information wasn’t being surfaced to them.</p>
+            <p>this project initially began as a simple refresh but once the team and I started talking to the users it soon came to light that the very nature of how we build ads was the biggest pain point — from uploading assets to managing them, it was all a mess. we started by asking a small sample of users to go through their workflow and noticed many of them weren’t aware of what they could edit within any given ad or how to manage assets. this was a design failure of the tool, as this information wasn’t being surfaced to them.</p>
 
-            <p>this led us to a tricky solve to display what things could be added/manipulated within an ad but not clutter things as to make it distracting.</p>
+            <p>we needed to find a good resolution to a tricky problem by showing what could be added or manipulated within an ad while keeping users from being overwhelmed.</p>
 
-            <p>we resolved this by reworking the tool to use a navigation system derived from a computer file system and popular code editors — this made it easy for users to see what options were available and leaned into things they were currently using along with letting us organize the information presented to users so they could easily and efficiently work through creating an ad on our platform.</p>
+            <p>we resolved this by reworking the tool to use a design paradigm found in code editors. this made it easier for users to see what options were available while leaning into tools they were already comfortable with using. by doing this, it let us better organize the information we were presenting to users so they could easily and efficiently create and distribute ads on our platform.</p>
 
             <img
               src="/assets/imgs/hymnal-ad-fields.jpg"
-              alt="the hymnal apps asset field"
+              alt="the hymnal apps asset field view"
             />
-            <cite>this is hymnals asset population fields, users would add assets to build our ads from this view</cite>
+            <cite>this is hymnals asset population fields, users add assets to build our ads from this view</cite>
 
             <img
               src="/assets/imgs/hymnal-ad-code.jpg"
-              alt="the hymnal apps main screen"
+              alt="the hymnal apps code view"
             />
-            <cite>this is hymnals code panel, users would add fuctionality and interactivity to our ads from this view</cite>
+            <cite>this is hymnals code view, users add fuctionality and interactivity to our ads from this view</cite>
 
             <img
               src="/assets/imgs/hymnal-ad-split-view.jpg"
-              alt="the hymnal apps main screen"
+              alt="the hymnal apps asset and code split view"
             />
             <cite>a split view to help users when hopping between assets and code views in quick succession</cite>
-
-            <h5>favorite change</h5>
-
-            <p>with the updated user flow and work process we were able to reduce the time it takes to create ads by about half; the most dramatic was an ad that had taken about 4 hours to make was cut down to about 1.5 hours.</p>
 
             <img
               src="/assets/imgs/hymnal-ad-testing.jpg"
               alt="the hymnal apps main screen"
             />
-            <cite>all of hymnals ads were responsive and we needed a way to test how they looked at various breakpoints</cite>
+            <cite>all of hymnals ads are built to be responsive and this is how we test how they looked at various breakpoints</cite>
 
             <h5>lessons learned</h5>
 
             <p>
               <ul>
-                <li>people have a hard time articulating issues with interfaces they’ve grown accustomed to using so proper context is needed to fish for what is bothering users.</li>
-                <li>be ready for ideas to fail, assumptions are only as good as their testing and while you have ideas you think will have legs, some user testing will tell you otherwise.</li>
-                <li>failure is a great tool in learning and is often how I came to some of my best ideas.</li>
+                <li>users have a hard time articulating issues with interfaces they’ve grown accustomed to, so pinpointing needs through questions is crucial to help gaining context.</li>
+                <li>be ready for ideas to fail. assumptions are only as good as their testing and while you anticipate your ideas to work, user testing will tell you otherwise.</li>
+                <li>failure is a great tool in learning and is often how teams arrive at some of the best solutions.</li>
               </ul>
             </p>
 
@@ -121,8 +118,8 @@ export default function Main() {
 
             <p>
               <ul>
-                <li>this redesign focused mostly on the creation and edit process and other parts of the app have not been addressed such as information organization, traversale within the system and other critical path issues within the experience.</li>
-                <li>looking into what this would be like on mobile — even as just a read only or cut down feature set since it doesn’t need parity with the desktop version.</li>
+                <li>this redesign focused mostly on the creation and edit process and other parts of the app have not been addressed such as information organization, traversal within the system and other critical path issues within the experience.</li>
+                <li>looking into what this would be like on mobile — even as just a read-only or with a diminished feature set since it doesn’t need parity with the desktop version.</li>
                 <li>further ui/user flow improvements to help speed things up further.</li>
               </ul>
             </p>
