14483716f86aa8284f40c7081569ba76aa0bc70f
Matt Sullivan <msullivanxi@gmail.com>
2020-08-17 01:42:32 -0400

    finished chorus

--- pages/projects/chorus.js
+++ pages/projects/chorus.js
@@ -42,66 +42,32 @@ export default function Main() {
             <img
               className={styles.fullWidth}
               src="/assets/imgs/chorus-header.jpg"
-              alt="the hymnal apps logo"
+              alt="the chorus apps logo"
             />
 
             <h3>chorus</h3>
-            <h4>this project is for vox medias home grown ad building platform, all the ads built for concert and all sites on chorus used this tool to build and serve up ads to millions of users.</h4>
+            <h4>this brief is for chorus, vox media’s content management system (cms). chorus helps power content across vox’s eight brands and over 300 websites daily.</h4>
 
-            <p>in 2016 the revenue product team set out to evaluate our internal tools and found one of the main product offerings had grown stale as a product and user flows were being affected negatively by it. The tool, which runs all of vox media's premium ads across multiple networks, had not grown with new and bigger demands and needed some rethinking to get things done in a more efficient and easier way.</p>
-            <p>the hope was to make creating and building ads much easier along with reducing the user friction at certain pain points in the creation by rethinking and modernizing the tool.</p>
+            <p>chorus was an ambitious project undertaken by vox media to help revolutionize the way stories were published on their platforms. this goal was overwhelmingly successful in comparison with offerings from its peers. i was fortunate enough to join the chorus team for over half a year, getting to contribute to story editing and asset management.</p>
 
             <img
               src="/assets/imgs/chorus-editor.jpg"
-              alt="the hymnal apps main screen"
+              alt="the chorus editor"
             />
-            <cite>the main screen when looking at a hymnal made ad</cite>
+            <cite>the peak at the chorus editor</cite>
 
             <h5>high level goals</h5>
 
             <p>
               <ul>
-                <li>reduce User Friction within the app</li>
-                <li>refresh User flow and rethink how new ads are built</li>
-                <li>reduce build times to decrease per ad cost</li>
+                <li>help rework flow of publishing stories to chorus</li>
+                <li>better maintain assets both within and out of the editor</li>
               </ul>
             </p>
 
-            <h5>jumping off point</h5>
+            <h5>final thoughts</h5>
 
-            <p>hymnal was brought into the world in 2014 and was a quick experiment by a few engineers as a proof of concept built using rails and bootstrap 2 without any design input (lolsob). Fast forward a few years and it was supporting nearly one hundred users and millions of dollars and grew to be one of vox media’s main lines of business. Due to user frustration hymnal was slated for a massive overhaul with a design lead rework.</p>
-
-            <h5>initial findings and resolutions</h5>
-
-            <p>this project initially began as a simple refresh but once the team and I started talking to the users it soon came to light that the very nature of how we build ads was the biggest pain point — from uploading assets to managing them, it was all a mess. We started by asking a small sample of users to go through their workflow and noticed many of them weren’t aware of what they could edit within any given ad or how to manage assets — a design failure of the tool, as this information wasn’t being surfaced to them.</p>
-
-            <p>this led us to a tricky solve to display what things could be added/manipulated within an ad but not clutter things as to make it distracting.</p>
-
-            <p>we resolved this by reworking the tool to use a navigation system derived from a computer file system and popular code editors — this made it easy for users to see what options were available and leaned into things they were currently using along with letting us organize the information presented to users so they could easily and efficiently work through creating an ad on our platform.</p>
-
-            <h5>favorite change</h5>
-
-            <p>with the updated user flow and work process we were able to reduce the time it takes to create ads by about half; the most dramatic was an ad that had taken about 4 hours to make was cut down to about 1.5 hours.</p>
-
-            <h5>lessons learned</h5>
-
-            <p>
-              <ul>
-                <li>people have a hard time articulating issues with interfaces they’ve grown accustomed to using so proper context is needed to fish for what is bothering users.</li>
-                <li>be ready for ideas to fail, assumptions are only as good as their testing and while you have ideas you think will have legs, some user testing will tell you otherwise.</li>
-                <li>failure is a great tool in learning and is often how I came to some of my best ideas.</li>
-              </ul>
-            </p>
-
-            <h5>future improvements</h5>
-
-            <p>
-              <ul>
-                <li>this redesign focused mostly on the creation and edit process and other parts of the app have not been addressed such as information organization, traversale within the system and other critical path issues within the experience.</li>
-                <li>looking into what this would be like on mobile — even as just a read only or cut down feature set since it doesn’t need parity with the desktop version.</li>
-                <li>further ui/user flow improvements to help speed things up further.</li>
-              </ul>
-            </p>
+            <p>since my time was so brief, i do not have much context to elaborate on my experience with the project. however, if you are interested in hearing more about my time with chorus, please do not hesitate to reach out.</p>
 
           </div>
         </motion.div>
