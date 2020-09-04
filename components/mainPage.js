7f8c7e65faa00f1086391a0fdc7d0c5e19db58a5
Matt Sullivan <msullivanxi@gmail.com>
2020-09-04 10:44:20 -0400

    made some small visual changes

--- components/mainPage.js
+++ components/mainPage.js
@@ -50,7 +50,7 @@ const MainPage = () => (
           },
         }}
       >
-        <h2>— projects</h2>
+        <h2>projects</h2>
         <ul>
           <Link href="/projects/design-process-and-leadership">
             <li><a>Process & Leadership</a></li>
@@ -90,7 +90,7 @@ const MainPage = () => (
           },
         }}
       >
-        <h2>— about</h2>
+        <h2>about</h2>
         <p>A little about me, I’m more of a cat person and have been living with my long term roomate <a target="_blank" href="//instagram.com/uglybabykai/">Kai</a> for the last 5 years, I enjoy reading books (mostly on <a target="_blank" href="//amazon.com/Very-Serious-Cookbook-Contra-Wildair/dp/071487602X">cooking</a> and <a target="_blank" href="https://shop.butterandscotch.com/collections/swag/products/signed-butter-scotch-cookbook">baking</a>) and finding weird books to get design inspiration from (shout out to <a target="_blank" href="//vimeo.com/55449250">Atlas of the New Dutch Water Defence Line</a>), really enjoy video games specifically life simulators and collector games (enjoy this <a target="_blank" href="//polygon.com/2014/8/21/6052553/how-to-catch-every-pokemon">story</a> of me as a youth playing Pokémon).</p>
         <p>You can find me on <a target="_blank" href="//twitter.com/mjsxi">Twitter</a> (I'm currently tweeting about Animal Crossing or ranting about design), <a target="_blank" href="//instagram.com/mjsxi/">Instagram</a>, and if you’re here for it <a target="_blank" href="//linkedin.com/in/matt-sullivan-5455656a/">Linkedin</a>. Feel free to reach out by <a href="mailto:msullivanxi@gmail.com">email</a> if you're looking to connect or just want to talk!</p>
       </motion.div>
